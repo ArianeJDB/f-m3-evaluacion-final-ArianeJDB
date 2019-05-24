df01068bede0e0e73765cdddc0621e711f31680f
arijdb <ariane.jurado@gmail.com>
2019-05-24 09:38:10 +0200

    fetch working alright

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
-
+import {fetchCharacters} from './services/fetchCharacters';
 import './App.scss';
 
 
 class App extends React.Component {
+ constructor(props) {
+   super(props);
+
+   this.state = {
+     characters: [],
+   }
+ }
+componentDidMount(){
+  this.getCharacters();
+}
+
+ getCharacters(){
+   fetchCharacters()
+    .then(data => {
+      console.log('data', data)
+      const newData = data.map((item, index) => {
+        return {...item, id:index};
+      });
+
+      this.setState({
+        characters: newData
+        
+      })
+      console.log('cha', this.state.characters);
+      
+    });
+    
+ }
  
   render() {
+    // const {characters} = this.state;
    return (
-     <h1>HOla</h1>
+     <React.Fragment>
+    <h1>Mis magos favoritos</h1>
+    <ul className="list">
+      {this.state.characters.map(item => {
+        return (
+          <li className="list-elements">
+            {item.name}
+          </li>
+        )
+      })}
+    </ul>
+    </React.Fragment>
    );
  }
 }
---
+++ src/services/fetchCharacters.js
@@ -0,0 +1,6 @@
+const ENDPOINT = 'http://hp-api.herokuapp.com/api/characters';
+
+const fetchCharacters = () => fetch(ENDPOINT)
+  .then(response => response.json())
+
+  export {fetchCharacters};

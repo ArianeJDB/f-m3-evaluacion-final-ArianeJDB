31191fc1ca9d2df7cfc43a249f5cec2d8d14f8aa
arijdb <ariane.jurado@gmail.com>
2019-05-27 09:14:11 +0200

    improve styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,10 +5,10 @@ class CharacterCard extends React.Component {
     return(
       <div className="magic-card">
         <img src={this.props.image} alt={this.props.name} className='img' />
-        <h2>{this.props.name}</h2>
+        <h2 className="name">{this.props.name}</h2>
         <div className='house-container'>
           <img className="house-img" alt={this.props.house} src={this.props.house === 'Gryffindor' ? 'https://images-na.ssl-images-amazon.com/images/I/81mv7IXpc6L._SY606_.jpg' : this.props.house === 'Slytherin' ? 'https://images-na.ssl-images-amazon.com/images/I/61n%2B3Bsn6PL._SY606_.jpg': this.props.house === 'Hufflepuff' ? 'https://i.ebayimg.com/images/i/282998706270-0-1/s-l1000.jpg' : this.props.house === 'Ravenclaw' ? 'https://elenanofriki.com/3664-large_default/placa-metalica-ravenclaw-harry-potter.jpg' : <p>No es de Hogwarts</p>}/>
-        <p>{this.props.house}</p>
+        <p className="house">{this.props.house}</p>
         </div>
         <Link to={`/detail/${this.props.id}`} className="more">Ver más</Link>
       </div>
